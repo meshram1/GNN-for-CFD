37f277c47a152d667393f11ca9ed4d68c431576d
aditya <adityakm2003@gmail.com>
2026-01-27 06:51:14 -0500

    Contains a bunch of model to run our GNN simulaiton

---
+++ models.py
@@ -0,0 +1,94 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import MessagePassing
+
+class PoolModel(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        # Initial Graph Convolution
+        self.conv1 = GCNConv(data.num_node_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+
+        # Regression head to predict your CFD values (e.g., Ux, Uy, P)
+        self.out = torch.nn.Linear(hidden_channels, 5)
+
+    def forward(self, data):
+        # 1. Obtain node embeddings
+
+        x, edge_index= data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+
+        # 2. Node-level output (for the full 50,480 vector prediction)
+        return self.out(x)
+#%%
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = PoolModel(64).to(device)
+data = data.to(device)
+data_plus = data_plus.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+model.train()
+total_loss = 0
+for epoch in range(200):
+
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.mse_loss(out, data_plus.x)
+    loss.backward()
+    optimizer.step()
+    total_loss += loss.item()
+
+    # Print average loss for the epoch
+    avg_loss = total_loss / (epoch+1)
+    print(f"Epoch {epoch+1:03d} | Average Loss: {avg_loss:.6f}")
+#%%
+class GNNMessagePassing(MessagePassing):
+    def __init__(self, node_input, edge_in, hidden_channels):
+        super(GNNMessagePassing, self).__init__(aggr='add')
+
+        self.msg_mlp = Sequential(
+            nn.Linear(2*hidden_channels + edge_in, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, hidden_channels),
+        )
+        self.upd_mlp = Sequential(
+            nn.Linear(2*hidden_channels, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, hidden_channels),
+        )
+
+    def forward(self, x=x, edge_index=edge_index, edge_attr=edge_attr):
+        return self.propagate(edge_index, x=x, edge_attr=edge_attr)
+
+    def message(self,x_i, x_j,edge_attr):
+        combined = torch.cat([x_i, x_j, edge_attr], dim=-1)
+        return self.msg_mlp(combined)
+
+    def update(self, aggr_out, x):
+        combined = torch.cat([x, aggr_out], dim=-1)
+        return self.upd_mlp(combined)
+#%%
+#using the message passing model
+
+class FlowPredictor(torch.nn.Module):
+    def __init__(self,hidden_channels):
+        super().__init__()
+        self.num_layers = 4
+        self.layers = torch.nn.ModuleList()
+
+        for _ in range(self.num_layers):
+            self.layers.append(GNNMessagePassing(node_input=hidden_channels, edge_in=3, hidden_channels=hidden_channels)) ## what sort of layers do we have in our module list.
+        self.encoder = nn.Linear(5, hidden_channels) # [x, y, u, v, p] -> 64
+#        self.processor = GNNMessagePassing(node_input=hidden_channels, edge_in=3, hidden_channels=hidden_channels)
+        self.decoder = nn.Linear(hidden_channels, 5) # hidden_channels -> [u, v, p]
+
+    def forward(self, data):
+        h = self.encoder(data.x)
+        for layer in self.layers:
+            h_update = layer(h, data.edge_index, data.edge_attr)
+
+        h = h + h_update
+        return self.decoder(h)
